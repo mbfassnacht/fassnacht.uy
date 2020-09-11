110e70af4f00057eb38120f08bbee169508e13da
Maximo Belen <maximo.fassnacht@exozet.com>
2020-09-11 23:07:45 +0200

    add buil;d

---
+++ build/precache-manifest.11d0eb10cd33541096789f9fce42ce24.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "659607fe214c4e9f871b980a171fac8f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "99153e1ef13dd92af1a2",
+    "url": "/static/css/main.636d5dc4.chunk.css"
+  },
+  {
+    "revision": "14060a56f1ac7ce53252",
+    "url": "/static/js/2.eecf1a6a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.eecf1a6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99153e1ef13dd92af1a2",
+    "url": "/static/js/main.8f2f6cbf.chunk.js"
+  },
+  {
+    "revision": "072d210fa73aec2277bc",
+    "url": "/static/js/runtime-main.c4092e1a.js"
+  }
+]);
